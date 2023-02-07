ec4086d75cc0c5771070b38b032df15c10552b93
Will Farris <9190155+WillFarris@users.noreply.github.com>
2023-02-06 18:49:28 -0800

    impl Uniform for mat3, mat2, float

--- src/graphics/uniform.rs
+++ src/graphics/uniform.rs
@@ -1,6 +1,6 @@
 use std::ffi::CString;
 
-use cgmath::{Vector3, Vector2, Matrix4};
+use cgmath::{Vector3, Vector2, Matrix4, Matrix3, Matrix2};
 
 use super::shader::Shader;
 
@@ -15,10 +15,17 @@ impl Uniform for Matrix4<f32> {
     }
 }
 
-impl Uniform for Vector2<f32> {
+impl Uniform for Matrix3<f32> {
     fn set_as_uniform(&self, shader: &Shader, name: &'static str) {
         let c_name = CString::new(name).unwrap();
-        shader.set_vec2(c_name.as_c_str(), self);
+        shader.set_mat3(c_name.as_c_str(), self);
+    }
+}
+
+impl Uniform for Matrix2<f32> {
+    fn set_as_uniform(&self, shader: &Shader, name: &'static str) {
+        let c_name = CString::new(name).unwrap();
+        shader.set_mat2(c_name.as_c_str(), self);
     }
 }
 
@@ -26,5 +33,19 @@ impl Uniform for Vector3<f32> {
     fn set_as_uniform(&self, shader: &Shader, name: &'static str) {
         let c_name = CString::new(name).unwrap();
         shader.set_vec3(c_name.as_c_str(), self);
+    }
+}
+
+impl Uniform for Vector2<f32> {
+    fn set_as_uniform(&self, shader: &Shader, name: &'static str) {
+        let c_name = CString::new(name).unwrap();
+        shader.set_vec2(c_name.as_c_str(), self);
+    }
+}
+
+impl Uniform for f32 {
+    fn set_as_uniform(&self, shader: &Shader, name: &'static str) {
+        let c_name = CString::new(name).unwrap();
+        shader.set_float(c_name.as_c_str(), *self);
     }
 }
